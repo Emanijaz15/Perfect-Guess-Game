f16daafea6f630e2e43e10990d9ed1ce37e9abd1
Emanijaz15 <eijaz7357@gmail.com>
2025-06-25 17:12:39 +0500

    Update guessgame.py

--- guessgame.py
+++ guessgame.py
@@ -1,13 +1,15 @@
 import random
 num=random.randint(1,100)
+guess=0
 while True:
+        guess+=1
         user=int(input("Guess the number: "))
         if user == num:
             print("Congrats!! You guessed it!!")
+            print(f"your guesses are {guess}")
             break
         elif user < num:
             print("Too low! Try again.")
         else:
             print("Too high! Try again.")
-
 
